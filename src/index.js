e57a01a955829a54b6637b2364d7afbadd10dc1c
Erpreet <preetkaurchd15@gmail.com>
2020-09-14 12:00:56 -0600

    store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {createStore} from 'redux';
+import toDosReducer from './reducers/todos';
+import { addNewToDo } from './actions/todos';
 
+const store = createStore(toDosReducer);
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(addNewToDo("Research Redux"));
+store.dispatch(addNewToDo("Research Redux"));
 
 ReactDOM.render(
   <React.StrictMode>
