cfeda5a4018412cfffc3652f8f928e99bbf29c9d
Erpreet <preetkaurchd15@gmail.com>
2020-09-14 15:07:08 -0600

    remove-to-do

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -2,7 +2,15 @@ const addNewToDo = toDoContent => {
     return{
         type: 'ADD_NEW_TO_DO', //this is the name of the ACTION
         payload: toDoContent // this is the DATA we are passing through to the reducer
-    }
-};
+    };
+}
 
-export {addNewToDo};
+const removeToDo = id => {
+    return {
+        type: 'REMOVE TO DO',
+        payload: id
+    };
+
+}
+
+export {addNewToDo, removeToDo};
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,13 +1,23 @@
+
+import { v4 as uuidv4 } from 'uuid';
+
 const toDosReducer = (state = [], action ) => {
     switch(action.type){
-        case 'ADD NEW TO DO':
+        case 'ADD_NEW_TO_DO':
             const newTask = {
+                id: uuidv4(),
                 task: action.payload
 
             }
             const newToDoList = [...state];
             newToDoList.push(newTask);
             return newToDoList;
+
+            case 'REMOVE_TO_DO':
+                const updatedToDoList = state.filter(
+                    toDo => toDo.id !== action.payload
+                );
+                return updatedToDoList;
             default:
                 return state;
     }
