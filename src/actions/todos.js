f984c8246e6d8961c6bfc8eeda9793a96c8358dc
Erpreet <preetkaurchd15@gmail.com>
2020-09-14 11:18:57 -0600

    basic initial commit

---
+++ src/actions/todos.js
@@ -0,0 +1,6 @@
+const addNewToDo = toDoContent => {
+    return{
+        type: 'ADD_NEW_TO_DO', //this is the name of the ACTION
+        payload: toDoContent // this is the DATA we are passing through to the reducer
+    }
+};
